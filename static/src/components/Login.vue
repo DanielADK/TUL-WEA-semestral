de5210cc1ee6cd9cfc5c685e7b3407af28dd47f0
Daniel Adámek <daniel.adamek123@icloud.dom>
2023-10-22 12:53:49 +0200

    login form aligned

--- static/src/components/Login.vue
+++ static/src/components/Login.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="container mt-5">
     <div class="row justify-content-center">
-      <div class="col-md-4">
+      <div class="col-md-4 col-sm-12">
         <h2>Login</h2>
         <form @submit.prevent="handleLogin">
-          <div class="mb-3 row">
+          <div class="mb-3">
             <label for="username">Username</label>
             <input type="text" class="form-control" id="username" placeholder="Enter username" v-model="username" required>
           </div>
-          <div class="mb-3 row">
+          <div class="mb-3">
             <label for="password">Password</label>
             <input type="password" class="form-control" id="password" placeholder="Enter password" v-model="password" required>
           </div>
@@ -77,5 +77,7 @@ export default {
 </script>
 
 <style scoped>
-
+button {
+  width: 100%;
+}
 </style>
