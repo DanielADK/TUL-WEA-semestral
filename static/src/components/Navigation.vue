64709dc95d7628953eb7dd70b352874c3a061aa2
Daniel Adámek <daniel.adamek123@icloud.dom>
2023-10-22 13:23:34 +0200

    navigation -> login/logout button

--- static/src/components/Navigation.vue
+++ static/src/components/Navigation.vue
@@ -1,26 +1,50 @@
-<script setup>
-
-</script>
-
 <template>
   <nav class="navbar bg-primary navbar-expand-lg mb-4" data-bs-theme="dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">TODO LIST</a>
+      <a class="navbar-brand" href="#"><i class="bi bi-journal-text"></i> TODO LIST</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <router-link class="nav-link" to="/">Home</router-link>
-          <router-link class="nav-link" to="/login">Login</router-link>
+        <ul class="navbar-nav me-auto">
+          <router-link class="nav-item nav-link" to="/">Home</router-link>
+        </ul>
+        <div>
+          <router-link v-if="!isAuthenticated" to="/login" class="btn btn-light nav-item">Login</router-link>
+          <button v-else @click="handleLogout" class="btn btn-danger nav-item">Logout</button>
         </div>
       </div>
     </div>
   </nav>
 </template>
 
-<style scoped>
+<script lang="ts">
+import {mapActions, mapGetters} from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["isAuthenticated"])
+  },
+  methods: {
+    ...mapActions(["addAlert", "logout"]),
+    async handleLogout() {
+      await this.logout();
+      await this.addAlert({
+        message: "You successfully logout.",
+        type: "success"
+      });
+      this.$router.push("/");
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.navbar {
+  box-shadow: 0 0 10px #B5B5B5;
+}
 .navbar-brand {
   font-weight: bold;
 }
+
 </style>
--- static/src/router.js
+++ static/src/router.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LoginScreen from "@/components/Login.vue";
 import Home from '@/components/Home.vue';
+import Dashboard from "@/components/Dashboard.vue";
+import store from "@/store";
 
 const routes =[
     {
@@ -12,12 +14,33 @@ const routes =[
         path: "/login",
         name: "login",
         component: LoginScreen
+    },
+    {
+        path: "/dashboard",
+        name: "dashboard",
+        component: Dashboard,
+        meta: { requiresAuth: true }
     }
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (!store.getters.isAuthenticated) {
+            next({
+                path: "/login",
+                query: { redirect: to.fullPath }
+            });
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
 })
 
 export default router
