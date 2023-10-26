6bfc790a130463156cb12a2857de83ac890eea73
Daniel Adámek <daniel.adamek123@icloud.dom>
2023-10-26 19:57:41 +0200

    token expiration

--- static/src/App.vue
+++ static/src/App.vue
@@ -11,8 +11,42 @@
 import { mapState } from "vuex";
 import Navigation from "@/components/Navigation.vue";
 import AlertList from "@/components/AlertList.vue";
+import {onMounted} from "vue";
+import store from "@/store";
+import router from "@/router";
 export default {
   components: {AlertList, Navigation},
+  mounted() {
+    const token = store.state.user?.token; // předpokládáme, že token je uložen ve Vuex state pod objektem 'user'
+
+    function isTokenExpired(token: string) {
+      try {
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+
+        const { exp } = JSON.parse(jsonPayload);
+
+        if (!exp) {
+          return false;
+        }
+
+        const currentUnixTime = Math.floor(new Date().getTime() / 1000);
+        return currentUnixTime > exp;
+      } catch (e) {
+        return false;
+      }
+    }
+
+    if (token && isTokenExpired(token)) {
+      console.log("Token has expired, need to refresh or logout");
+      store.dispatch("logout");
+      store.dispatch("addAlert", { message: "Your session expired. Please login again", type: "warning" });
+      router.push("/login");
+    }
+  },
 }
 </script>
 
